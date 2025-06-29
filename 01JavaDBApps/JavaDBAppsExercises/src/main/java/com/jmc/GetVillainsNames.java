82b74037beb212aa6cae0f15da216676f711b221
tproykov <tproykov@gmail.com>
2025-06-29 16:36:45 +0100

    update

--- 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/GetVillainsNames.java
+++ 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/GetVillainsNames.java
@@ -1,6 +1,8 @@
 package com.jmc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class GetVillainsNames {
@@ -9,16 +11,22 @@ public class GetVillainsNames {
 
         Connection connection = DatabaseUtil.getConnection("minions_db");
 
+        PreparedStatement preparedStatement = connection
+                .prepareStatement("""
+                        SELECT v.id, v.name, COUNT(*) count_minions
+                        FROM villains v
+                        INNER JOIN minions_villains mv ON v.id = mv.villain_id
+                        GROUP BY v.id
+                        HAVING count_minions > 15
+                        ORDER BY count_minions DESC;""");
 
+        ResultSet resultSet = preparedStatement.executeQuery();
 
+        while (resultSet.next()) {
+            String name = resultSet.getString("name");
+            int countMinions = resultSet.getInt("count_minions");
 
-
-
-        
-
+            System.out.printf("%s - %d\n", name, countMinions);
+        }
     }
-
-
-
-
 }
