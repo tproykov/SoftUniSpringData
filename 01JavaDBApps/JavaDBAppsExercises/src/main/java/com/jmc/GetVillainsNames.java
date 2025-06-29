417308673c0b144a620f2aeb76f24a8b1339ff0f
tproykov <tproykov@gmail.com>
2025-06-29 16:14:36 +0100

    Create GetVillainsNames.java

---
+++ 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/GetVillainsNames.java
@@ -0,0 +1,24 @@
+package com.jmc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class GetVillainsNames {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DatabaseUtil.getConnection("minions_db");
+
+
+
+
+
+
+        
+
+    }
+
+
+
+
+}
