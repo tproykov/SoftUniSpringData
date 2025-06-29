3809ba41876e841d9519a9c2a44844b6b3bddd87
tproykov <tproykov@gmail.com>
2025-06-29 21:17:41 +0100

    Update GetMinionNames.java

--- 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/GetMinionNames.java
+++ 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/GetMinionNames.java
@@ -12,11 +12,19 @@ public class GetMinionNames {
 
         Connection connection = DatabaseUtil.getConnection("minions_db");
 
-        PreparedStatement statement = connection
-                .prepareStatement("""
-                        SELECT v.name
-                        FROM villains v
-                        WHERE v.id = ?;""");
+        boolean villainExists = printVillain(connection, villainId);
+
+        if (villainExists) {
+            printMinions(connection, villainId);
+
+        }
+    }
+
+    private static boolean printVillain(Connection connection, String villainId) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("""
+                SELECT v.name
+                FROM villains v
+                WHERE v.id = ?;""");
 
         statement.setString(1, villainId);
 
@@ -25,9 +33,29 @@ public class GetMinionNames {
         if (resultSet.next()) {
             String name = resultSet.getString("name");
             System.out.println(name);
+            return true;
         } else {
             System.out.println("Villain with this ID does not exist.");
+            return false;
         }
+    }
 
+    private static void printMinions(Connection connection, String villainId) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("""
+                SELECT DISTINCT m.name, m.age
+                FROM minions_villains mv
+                JOIN minions m ON mv.minion_id = m.id
+                WHERE mv.villain_id = ?;""");
+
+        statement.setString(1, villainId);
+
+        int order = 1;
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            String minionName = resultSet.getString(1);
+            int age = resultSet.getInt(2);
+
+            System.out.printf("%d. %s %d\n", order++, minionName, age);
+        }
     }
 }
