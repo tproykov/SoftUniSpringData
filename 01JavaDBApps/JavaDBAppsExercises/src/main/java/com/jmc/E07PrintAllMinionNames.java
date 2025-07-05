c9661984ae0abf79eff73e73742a653771c3366b
tproykov <tproykov@gmail.com>
2025-07-05 18:36:30 +0100

    Update E07PrintAllMinionNames.java

--- 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/E07PrintAllMinionNames.java
+++ 01JavaDBApps/JavaDBAppsExercises/src/main/java/com/jmc/E07PrintAllMinionNames.java
@@ -13,29 +13,27 @@ public class E07PrintAllMinionNames {
 
         Connection connection = DatabaseUtil.getConnection("minions_db");
 
-        // getMinionsInMemory(connection);
+        getMinionsInMemory(connection);
 
-        PreparedStatement preparedStatement = connection.prepareStatement("""
-                SELECT m.name FROM minions m;""", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        int count = 0;
-        while (resultSet.next()) {
-            count++;
-        }
-
-        for (int i = 0; i < count; i++) {
-            if (i % 2 == 0) {
-                resultSet.absolute(i/2 + 1);
-            }
-            else {
-                resultSet.absolute(count - (i - 1) / 2);
-            }
-
-            String minionName = resultSet.getString(1);
-            System.out.println(minionName);
-        }
+//        CRAWLING THE RESULTSET
+//        PreparedStatement preparedStatement = connection.prepareStatement("""
+//                SELECT m.name FROM minions m;""", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//
+//        ResultSet resultSet = preparedStatement.executeQuery();
+//        int count = 0;
+//        while (resultSet.next()) {
+//            count++;
+//        }
+//        for (int i = 0; i < count; i++) {
+//            if (i % 2 == 0) {
+//                resultSet.absolute(i/2 + 1);
+//            }
+//            else {
+//                resultSet.absolute(count - (i - 1) / 2);
+//            }
+//            String minionName = resultSet.getString(1);
+//            System.out.println(minionName);
+//        }
     }
 
     private static void getMinionsInMemory(Connection connection) throws SQLException {
