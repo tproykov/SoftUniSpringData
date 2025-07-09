0ded6536a9676c970ddf8f558aee7ad23d5fb39e
tproykov <tproykov@gmail.com>
2025-07-09 20:48:33 +0100

    Update Main.java

--- 07_Hibernate_intro/src/main/java/com/jmc/Main.java
+++ 07_Hibernate_intro/src/main/java/com/jmc/Main.java
@@ -1,7 +1,13 @@
 package com.jmc;
 
+import org.hibernate.cfg.Configuration;
+
 public class Main {
+
     public static void main(String[] args) {
+
+        Configuration config = new Configuration();
+        config.configure("hibernate.cfg.xml");
 
 
     }
