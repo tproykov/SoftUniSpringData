afb0e976d4034124138876e284e16940036e436d
tproykov <tproykov@gmail.com>
2025-07-10 21:42:15 +0100

    Update Student.java

--- 07_Hibernate_intro/src/main/java/com/jmc/entities/Student.java
+++ 07_Hibernate_intro/src/main/java/com/jmc/entities/Student.java
@@ -2,9 +2,24 @@ package com.jmc.entities;
 
 public class Student {
 
-    public static void main(String[] args) {
+    private long id;
+    private String name;
 
+    public Student() {}
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
