307ba350ec8463ba60479723eb818f081793587d
tproykov <tproykov@gmail.com>
2025-07-11 19:29:23 +0100

    update

--- 07_Hibernate_intro/src/main/java/com/jmc/Main.java
+++ 07_Hibernate_intro/src/main/java/com/jmc/Main.java
@@ -1,5 +1,6 @@
 package com.jmc;
 
+import com.jmc.entities.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,11 +10,26 @@ public class Main {
     public static void main(String[] args) {
 
         Configuration config = new Configuration();
-        config.configure("hibernate.cfg.xml");
+        config.configure();
 
         SessionFactory sessionFactory = config.buildSessionFactory();
-        Session currentSession = sessionFactory.getCurrentSession();
+        Session currentSession = sessionFactory.openSession();
 
+        currentSession.getTransaction().begin();
+
+        // CREATE STUDENT
+        // Student student = new Student();
+        // student.setName("John Doe");
+        // currentSession.persist(student);
+
+        // FIND SPECIFIC STUDENT BY ID
+        // Student found = currentSession.find(Student.class, 1);
+        // System.out.println(found.getName());
+
+
+
+        currentSession.getTransaction().commit();
+        currentSession.close();
 
     }
 }
