333c0ae5b5cbd29963b0968c0a129d1ca07b4240
tproykov <tproykov@gmail.com>
2025-07-12 22:07:45 +0100

    Update Main.java

--- 07_Hibernate_Intro2/src/main/java/com/jmc/Main.java
+++ 07_Hibernate_Intro2/src/main/java/com/jmc/Main.java
@@ -1,6 +1,6 @@
 package com.jmc;
 
-import com.jmc.entities.Vehicle;
+import com.jmc.entities.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -14,6 +14,32 @@ public class Main {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-emf");
         EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Bike bike = new Bike();
+        bike.setModel("BMX");
+        em.persist(bike);
+
+        Car car = new Car();
+        car.setModel("BMW");
+        car.setSeas(4);
+        car.setFuelType("Gasoline");
+        em.persist(car);
+
+        Plane plane = new Plane();
+        plane.setModel("Boeing 747");
+        plane.setPassengerCapacity(1000);
+        plane.setFuelType("Gasoline");
+        em.persist(plane);
+
+        Truck truck = new Truck();
+        truck.setModel("Volvo 140");
+        truck.setLoadCapacity(2.5);
+        em.persist(truck);
+
+
+
 
         TypedQuery<Vehicle> query = em.createQuery("select v from Vehicle v", Vehicle.class);
         List<Vehicle> vehicles = query.getResultList();
