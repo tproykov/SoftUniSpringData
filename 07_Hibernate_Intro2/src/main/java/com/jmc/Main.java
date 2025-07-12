7ef3b84fda83462f6a8e787975ecdfca97211e52
tproykov <tproykov@gmail.com>
2025-07-12 11:54:03 +0100

    update

---
+++ 07_Hibernate_Intro2/src/main/java/com/jmc/Main.java
@@ -0,0 +1,25 @@
+package com.jmc;
+
+import com.jmc.entities.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("my-emf");
+        EntityManager em = emf.createEntityManager();
+        // em.getTransaction().begin();
+        // Student student = new Student("Teo");
+        // em.persist(student);
+        // em.getTransaction().commit();
+
+
+
+
+
+
+    }
+}
