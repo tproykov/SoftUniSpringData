f4242b9bbe3f5a9ab33ffe6e0f1b91b29dd58020
tproykov <tproykov@gmail.com>
2025-07-13 18:02:17 +0100

    Update Main.java

--- 07_Hibernate_Intro2/src/main/java/com/jmc/Main.java
+++ 07_Hibernate_Intro2/src/main/java/com/jmc/Main.java
@@ -6,7 +6,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
 
 public class Main {
 
@@ -45,6 +46,37 @@ public class Main {
         List<Vehicle> vehicles = query.getResultList();
 
         System.out.println(vehicles.size());
+
+        // ==============================================
+
+        em.getTransaction().begin();
+
+        Label label = new Label();
+        label.setName("Smooth and long hair");
+
+        ProductionBatch batch = new ProductionBatch();
+        batch.setDate(LocalDate.now());
+
+        Ingredient ingredient1 = new Ingredient();
+        ingredient1.setName("Salt");
+
+        Ingredient ingredient2 = new Ingredient();
+        ingredient2.setName("Lavender");
+
+        Ingredient ingredient3 = new Ingredient();
+        ingredient3.setName("Camomile");
+
+        Shampoo shampoo = new Shampoo();
+        shampoo.setBrand("The best shampoo company");
+        shampoo.setLabel(label);
+        shampoo.setBatch(batch);
+        shampoo.setIngredients(new HashSet<Ingredient>(List.of(ingredient1, ingredient2, ingredient3)));
+        
+        em.persist(shampoo);
+
+
+        em.getTransaction().commit();
+        em.close();
 
 
     }
