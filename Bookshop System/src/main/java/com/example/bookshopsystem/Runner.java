dc02bd6534b29667dc9369b50a1e823aa9c4c07c
tproykov <tproykov@gmail.com>
2025-07-19 23:52:20 +0300

    commit

--- Bookshop System/src/main/java/com/example/bookshopsystem/Runner.java
+++ Bookshop System/src/main/java/com/example/bookshopsystem/Runner.java
@@ -1,18 +1,37 @@
 package com.example.bookshopsystem;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 
 @Component
 public class Runner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello World!");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input);
+        List<String> categories = readSeedFiles("categories.txt");
+        List<String> booksData = readSeedFiles("books.txt");
+
+
+
+        // 2. Prepare repository files + services for seeds and queries
     }
+
+    private List<String> readSeedFiles(String filename) throws IOException {
+        ClassPathResource resource = new ClassPathResource(filename);
+
+        try (InputStream inputStream = resource.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            return bufferedReader.lines().toList();
+        }
+    }
+
 }
