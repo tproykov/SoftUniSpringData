a259d81c96ff7db87aaa7b4af09a81672c79e358
tproykov <tproykov@gmail.com>
2025-07-20 22:24:18 +0300

    commit

--- Bookshop System/src/main/java/com/example/bookshopsystem/dtos/BookInputDto.java
+++ Bookshop System/src/main/java/com/example/bookshopsystem/dtos/BookInputDto.java
@@ -1,4 +1,16 @@
 package com.example.bookshopsystem.dtos;
 
+import com.example.bookshopsystem.enums.EditionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class BookInputDto {
+    private final String title;
+    private final BigDecimal price;
+    private final Integer copies;
+    private final EditionType editionType;
+    private final LocalDate releaseDate;
+
+
 }
