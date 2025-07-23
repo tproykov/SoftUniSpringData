6c465013fd0b6730f0bf47eaab57716b9b103fb0
tproykov <tproykov@gmail.com>
2025-07-23 07:57:42 +0300

    Update Device.java

--- Exam_Preparation/Mobiles/src/main/java/softuni/exam/entities/Device.java
+++ Exam_Preparation/Mobiles/src/main/java/softuni/exam/entities/Device.java
@@ -1,8 +1,6 @@
 package softuni.exam.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import softuni.exam.enums.DeviceType;
 
 @Entity
@@ -18,10 +16,55 @@ public class Device extends BaseEntity {
     private Double price;
     @Column(name = "storage")
     private Integer storage;
-
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
     private Sale sale;
 
+    public String getBrand() {
+        return brand;
+    }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
+    public DeviceType getDeviceType() {
+        return deviceType;
+    }
 
+    public void setDeviceType(DeviceType deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Integer storage) {
+        this.storage = storage;
+    }
+
+    public Sale getSale() {
+        return sale;
+    }
+
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
 }
