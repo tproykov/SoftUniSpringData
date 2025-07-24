9f7776406969584fd2b813acf103bf4810fe2257
tproykov <tproykov@gmail.com>
2025-07-24 22:19:53 +0300

    Update XmlParserImpl.java

--- Exam_Preparation/Mobiles/src/main/java/softuni/exam/util/XmlParserImpl.java
+++ Exam_Preparation/Mobiles/src/main/java/softuni/exam/util/XmlParserImpl.java
@@ -1,11 +1,19 @@
 package softuni.exam.util;
 
+import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+
+import java.io.StringReader;
 
 public class XmlParserImpl implements XmlParser {
     @Override
     public <T> T fromXml(String xml, Class<T> tClass) throws JAXBException {
-        
-        return null;
+        JAXBContext jaxbContext = JAXBContext.newInstance(tClass);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        try (StringReader reader = new StringReader(xml)) {
+            return (T) unmarshaller.unmarshal(reader);
+        }
     }
 }
