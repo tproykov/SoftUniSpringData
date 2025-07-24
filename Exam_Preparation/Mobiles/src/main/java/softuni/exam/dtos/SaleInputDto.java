b67f162209f5ad9f7ddbd65e6318fc396dd5ccad
tproykov <tproykov@gmail.com>
2025-07-24 21:11:42 +0300

    Add validation and mapping for Sale import
    Introduced validation annotations to SaleInputDto fields and implemented validation and mapping logic in SaleServiceImpl for importing sales. The service now uses Gson, ModelMapper, and ValidationUtil to parse, validate, and persist Sale entities from input DTOs.

--- Exam_Preparation/Mobiles/src/main/java/softuni/exam/dtos/SaleInputDto.java
+++ Exam_Preparation/Mobiles/src/main/java/softuni/exam/dtos/SaleInputDto.java
@@ -1,6 +1,8 @@
 package softuni.exam.dtos;
 
 import com.google.gson.annotations.Expose;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 
@@ -8,9 +10,44 @@ public class SaleInputDto {
     @Expose
     private boolean discounted;
     @Expose
+    @NotNull
+    @Length(min = 7, max = 7)
     private String number;
     @Expose
+    @NotNull
     private String saleDate;
     @Expose
     private Long seller;
+
+    public boolean isDiscounted() {
+        return discounted;
+    }
+
+    public void setDiscounted(boolean discounted) {
+        this.discounted = discounted;
+    }
+
+    public @NotNull @Length(min = 7, max = 7) String getNumber() {
+        return number;
+    }
+
+    public void setNumber(@NotNull @Length(min = 7, max = 7) String number) {
+        this.number = number;
+    }
+
+    public @NotNull String getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(@NotNull String saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public Long getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Long seller) {
+        this.seller = seller;
+    }
 }
--- Exam_Preparation/Mobiles/src/main/java/softuni/exam/service/impl/SaleServiceImpl.java
+++ Exam_Preparation/Mobiles/src/main/java/softuni/exam/service/impl/SaleServiceImpl.java
@@ -1,8 +1,14 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.dtos.SaleInputDto;
+import softuni.exam.entities.Sale;
 import softuni.exam.repository.SaleRepository;
 import softuni.exam.service.SaleService;
+import softuni.exam.util.ValidationUtil;
+import softuni.exam.util.ValidationUtilImpl;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,9 +19,18 @@ import java.nio.file.Paths;
 public class SaleServiceImpl implements SaleService {
 
     private final SaleRepository repository;
+    private final Gson gson;
+    private final ValidationUtil validator;
+    private final ValidationUtilImpl validationUtilImpl;
+    private final ModelMapper modelMapper;
 
-    public SaleServiceImpl(SaleRepository repository) {
+    public SaleServiceImpl(SaleRepository repository, Gson gson,
+                           ValidationUtil validator, ValidationUtilImpl validationUtilImpl, ModelMapper modelMapper) {
         this.repository = repository;
+        this.gson = gson;
+        this.validator = validator;
+        this.validationUtilImpl = validationUtilImpl;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -31,6 +46,29 @@ public class SaleServiceImpl implements SaleService {
 
     @Override
     public String importSales() throws IOException {
+        SaleInputDto[] inputDtos = gson.fromJson(readSalesFileContent(), SaleInputDto[].class);
+        for (SaleInputDto inputDto : inputDtos) {
+            Sale createdSale = create(inputDto);
+
+
+
+        }
+
+
+
+
         return "";
+    }
+
+    private Sale create(SaleInputDto inputDto) {
+        if (!validator.isValid(inputDto)) return null;
+
+        try {
+            Sale sale = modelMapper.map(inputDto, Sale.class);
+            repository.save(sale);
+            return sale;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
