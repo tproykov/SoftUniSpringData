e9041c2b8f66ffccea2766a963f6f0682f59f71d
tproykov <tproykov@gmail.com>
2025-07-25 21:27:04 +0300

    Update Volcanologist.java

--- Exam_Preparation/Volcano/src/main/java/softuni/exam/models/entity/Volcanologist.java
+++ Exam_Preparation/Volcano/src/main/java/softuni/exam/models/entity/Volcanologist.java
@@ -1,25 +1,76 @@
 package softuni.exam.models.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "volcanologists")
 public class Volcanologist extends BaseEntity {
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name", nullable = false, unique = true)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name", nullable = false, unique = true)
     private String lastName;
 
-    @Column(name = "age")
+    @Column(name = "salary", nullable = false)
+    private Double salary;
+
+    @Column(name = "age", nullable = false)
     private Integer age;
 
+    @Column(name = "exploring_from")
+    private LocalDate exploringFrom;
 
+    @ManyToOne
+    @JoinColumn(name = "volcano_id", referencedColumnName = "id")
+    private Volcano volcano;
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getExploringFrom() {
+        return exploringFrom;
+    }
+
+    public void setExploringFrom(LocalDate exploringFrom) {
+        this.exploringFrom = exploringFrom;
+    }
+
+    public Volcano getVolcano() {
+        return volcano;
+    }
+
+    public void setVolcano(Volcano volcano) {
+        this.volcano = volcano;
+    }
 }
